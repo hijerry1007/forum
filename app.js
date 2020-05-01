f5beab768dd946d200dda97a98f9fdc976c095f4
Jerry Lai <jerrydavid123@hotmail.com>
2020-05-01 21:13:52 +0800

    Initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const db = require('./models') //引入資料庫
 const handlebars = require('express-handlebars')
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
