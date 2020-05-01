b2d4fe55fcca97508ea30ff7af21758c04efbff1
Jerry Lai <jerrydavid123@hotmail.com>
2020-05-01 22:00:11 +0800

    Install express-session, connect-flash and Confirm user password and unique email

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const app = express()
 const db = require('./models') //引入資料庫
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(3000, () => {
   console.log('app is running!')
