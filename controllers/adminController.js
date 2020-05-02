55a5881a028fe251b3054f58bf80e90ec6b6b840
Jerry Lai <jerrydavid123@hotmail.com>
2020-05-02 10:40:56 +0800

    create Restaurant model

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,38 @@
 const adminController = {
 
+  // 瀏覽所有
   getRestaurants: (req, res) => {
     return res.render('admin/restaurants')
+  },
+
+  //瀏覽一筆
+  getRestaurant: (req, res) => {
+
+  },
+
+  //get 表單 新增
+  createRestaurants: (req, res) => {
+
+  },
+
+  //新增
+  postRestaurants: (req, res) => {
+
+  },
+
+  //get 編輯
+  editRestaurant: (req, res) => {
+
+  },
+
+  //編輯
+  putRestaurant: (req, res) => {
+
+  },
+
+  //移除
+  deleteRestaurant: (req, res) => {
+
   }
 
 }
